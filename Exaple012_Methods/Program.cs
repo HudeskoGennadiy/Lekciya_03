b0e05f006d87b40c70da8c0dfd90e2c929cb9d0b
Gennadiy Khudesko <gennadiy.18224@gmail.com>
2022-10-01 18:52:57 +0300

    Example012_Methods solved

--- Exaple012_Methods/Program.cs
+++ Exaple012_Methods/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вид 1
+
+void Method1()
+{
+    Console.WriteLine("Автор Геннадий");
+}
+//Method1();
+
+
+// Вид2
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+//Method2("Текст сообщения");
+
+
+// Вид3
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+//Console.WriteLine(year);
+
+
+// Вид4
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+
+}
+string res = Method4(10, "z");
+Console.WriteLine(res);
+
+
+
+
